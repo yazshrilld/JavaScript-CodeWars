2be318def23dc17171147160e607bcc8b920b22a
yazshrilld <yazshrilld@gmail.com>
2022-11-24 15:47:54 +0100

    adding comment to remember how I solved my problem

--- algorithms/getCount.js
+++ algorithms/getCount.js
@@ -2,9 +2,13 @@
     Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or spaces.
 */
 function getCount(str) {
+    //create a variable and set it to zero 
     let countingLetter = 0;
+    //create a array of the vowels
     const vowelAlphabet = ['a','e','i','o','u'];
+    //create a loop to loop through the array of the arguement passed inside the function
     for(let checkEachSrting of str) {
+        //check if the vaues in the array is part of the 
         if(vowelAlphabet.includes(checkEachSrting)) {
             countingLetter += 1
         }
